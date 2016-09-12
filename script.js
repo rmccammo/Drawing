e358c922ba72684e83bafde5a73a311706ef8d8a
Robert <rlmccammon@live.com>
2016-09-12 13:01:11 -0500

    Can generate a grid of divs

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
 	console.log("Hello");
+
+	for(var x = 0; x < 16; x++){
+		var row = $('<div class="row"></div>')
+		row.appendTo('#wrapper');
+		for(var y = 0; y < 16; y++){
+			var unit = $('<div class="unit">Unit</div>');
+			//unit.appendTo('#wrapper');
+			unit.appendTo(row);
+		}
+	}
 });
