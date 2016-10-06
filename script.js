003ffb314964d24814c73de06ebf5acc60203883
Robert <rlmccammon@live.com>
2016-10-06 06:10:20 -0500

    Grid now fills the wrapper. Method to make the grid can now make a grid with any number of elements

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
 	console.log("Hello");
+	makeGrid(16);
 
-	for(var x = 0; x < 16; x++){
+});
+
+//Fill the wrapper with a grid of n divs
+function makeGrid(n){
+	for(var x = 0; x < n; x++){
 		var row = $('<div class="row"></div>')
 		row.appendTo('#wrapper');
-		for(var y = 0; y < 16; y++){
+		for(var y = 0; y < n; y++){
 			var unit = $('<div class="unit">Unit</div>');
 			//unit.appendTo('#wrapper');
 			unit.appendTo(row);
 		}
 	}
-});
+
+	var size = 960/n;
+	$(".unit").width(size).height(size);
+
+}
