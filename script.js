181c646385c674f2586672366444d5bfd260ddc4
Robert <rlmccammon@live.com>
2016-10-09 07:47:35 -0500

    Reset button now lets you change the size of the grid. Project is now complete

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	console.log("Hello");
 	makeGrid(16);
 
-
-
 	//When the reset button is clicked, clear the wrapper and
-	//make a new grid
+	//make a new grid of the requested size
 	$('#resetButton').click(function(){
 		$('#wrapper').empty();
-		makeGrid(16);
+
+		var newGridSize = prompt("Enter a new grid size");
+		makeGrid(newGridSize);
 	});
 });
 
@@ -18,7 +18,7 @@ function makeGrid(n){
 		var row = $('<div class="row"></div>')
 		row.appendTo('#wrapper');
 		for(var y = 0; y < n; y++){
-			var unit = $('<div class="unit">Unit</div>');
+			var unit = $('<div class="unit"></div>');
 			//unit.appendTo('#wrapper');
 			unit.appendTo(row);
 		}
